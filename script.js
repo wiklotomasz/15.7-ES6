f93c8c887e4e461b9416ea82fd27c365ae8632d2
Tomasz Wiklo <wikweb@gmail.com>
2018-03-07 21:48:36 +0100

    changes

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-StopWatch = React.createClass({
-    getInitialState: function() {
-        return {
-            running: false,
-            display: display,
-            this.reset(),
-            this.print(this.times);
-        };
-    },
+class StopWatch {
+    constructor(display) {
+        this.running = false;
+        this.display = display;
+        this.reset();
+        this.print(this.times);
+    }
     
-    reset: function() {
-        this.times({
+    reset() {
+        this.times = {
             minutes: 0,
             seconds: 0,
             miliseconds: 0
-        });
-    },
+        };
+    }
     
-    print: function() {
+    print() {
         this.display.innerText = this.format(this.times);
-    },
+    }
     
-    format: function(times) {
-        return {                                                                   (${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))});
-        }
-    },
-
-    start: function() {
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+    
+    start() {
         if (!this.running) {
             this.running = true;
-            this.watch = setInterval( () => this.step(), 10);
+            this.watch = setInterval( () => this.step(), 10 );
         }
-    },
+    }
     
-    step: function() {
+    step() {
         if (!this.running) return;
         this.calculate();
         this.print();
-    },
+    }
     
-    calculate: function() {
+    calculate() {
         this.times.miliseconds += 1;
         if (this.times.miliseconds >= 100) {
             this.times.seconds += 1;
@@ -47,49 +44,27 @@ StopWatch = React.createClass({
         if (this.times.seconds >= 60) {
             this.times.minutes += 1;
             this.times.seconds = 0;
-        }
-    },
+    }
+}
     
-    stop: function() {
+    stop() {
         this.running = false;
         clearInterval(this.watch);
     }
+}
 
-    pad0: function(value) {
-        var result = value.toString();
+function pad0(value) {
+        let result = value.toString();
         if (result.length < 2) {
             result = '0' + result;
         }
         return result;
     }
 
-    var stopwatch = new StopWatch(document.querySelector('.stopwatch'));
+const stopwatch = new StopWatch(document.querySelector('.stopwatch'));
 
-    var startButton = document.getElementById('start');
-    startButton.addEventListener('click', () => stopwatch.start());
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', () => stopwatch.start());
 
-    var stopButton = document.getElementById('stop');
-    stopButton.addEventListener('click', () => stopwatch.stop());
-
-    render: function() {
-        var styles = {
-            margin: '0 auto',
-            textAlign: 'center',
-            width: 90%;
-        };
-        
-        return (
-            <div style={styles}>
-                <nav className='controls'>
-                    <a href='#' className='button' id='start'>Start</a>
-                    <a href='#' className='button' id='stop'>Start</a>
-                </nav>
-                <div className='stopwatch'></div>
-                <ul className='results'></ul>
-            </div>
-        );
-    }    
-});
-
-var element = React.createElement(App);
-ReactDOM.render(element, document.getElementById('app'));
+let stopButton = document.getElementById('stop');
+stopButton.addEventListener('click', () => stopwatch.stop());
